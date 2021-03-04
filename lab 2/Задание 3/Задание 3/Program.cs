445b478ea9f89679e0dee156a8b5d5c44cd0a454
Fursanov <79316828+Fursanov@users.noreply.github.com>
2021-03-04 20:14:07 +0200

    Add files via upload

---
+++ lab 2/Задание 3/Задание 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Swap (System.Text.StringBuilder str, int a,int b)
+        {
+            char c = str[a];
+            str[a] = str[b];
+            str[b] = c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("программа перемешивает символы введённой строки.\nВведите начальную строку:");
+            Random rnd = new Random();
+            System.Text.StringBuilder str = new System.Text.StringBuilder();
+            str.AppendFormat(Console.ReadLine());
+            for (int i = 0; i < str.Length; i++)
+                Swap(str,i,rnd.Next(0, str.Length-1));
+            Console.WriteLine(str);
+        }
+    }
+}
