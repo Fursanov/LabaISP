61ebe5cc3181af1cd48ea195c638e700825a75e8
Fursanov <79316828+Fursanov@users.noreply.github.com>
2021-03-04 19:56:47 +0200

    Add files via upload

---
+++ lab2/Задание 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Программа генерирует равновероятно случайную строку строчных английских букв, длиной, введенной с клавиатуры.");
+            Console.Write("введите длину сироки");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Char[]A = new char[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                A[i] = Convert.ToChar(rnd.Next(97, 122));
+                Console.Write(A[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
